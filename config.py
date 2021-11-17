93607d967e4b27691f3712ec40b626126ff104c9
Илья <94282440+ilyalelechenko@users.noreply.github.com>
2021-11-18 00:10:44 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ from google.oauth2 import service_account
 import os
 from googleapiclient.discovery import build
 
-chat_id = '-748764953'
-token = '2129377067:AAECIAkGnDTJABdUqraDoIz1xL0Vv2mbLrc'
+chat_id = ''
+token = ''
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SERVICE_ACCOUNT_FILE = f'{os.getcwd()}\service.json'
+SERVICE_ACCOUNT_FILE = os.path.abspath('service.json')
 credentials = service_account.Credentials.from_service_account_file(
     SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 service = build('sheets', 'v4', credentials=credentials)
